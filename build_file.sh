c8c6e820ca9fee27e42f2980252c1a408cb7294c
hamidi <hamidahmed373@gmail.com>
2026-02-26 22:24:20 +0100

    misho

---
+++ build_file.sh
@@ -0,0 +1,15 @@
+# build_files.sh
+#!/bin/bash
+
+echo "BUILD START"
+
+# Install dependencies
+pip install -r requirements.txt
+
+# Collect static files
+python manage.py collectstatic --noinput
+
+# Run migrations (optional - be careful with this in production)
+# python manage.py migrate
+
+echo "BUILD END"
